714bf289634ae6981266b2d4a85c0e33a9bfa9b8
Leonardo Wijaya <leonardo.wijaya003@binus.ac.id>
2023-09-29 13:06:40 +0700

    (fix) Login title and subtitle

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,6 @@
 <x-auth-layout>
     <div class="col-span-2"></div>
-    <x-auth.form.box colSpan="col-span-4" title="Masuk Dashboard" subtitle="Yakin, kamu pasti bisa!" action="login" formClass="mt-9 space-y-5">
+    <x-auth.form.box colSpan="col-span-4" title="Yakin, kamu pasti bisa!" subtitle="Masuk Dashboard" action="login" formClass="mt-9 space-y-5">
         <x-auth.form.formInput label="Username" type="text" name="username" />
         <x-auth.form.formInput label="Password" type="password" name="password" />
     </x-auth.form.box>
