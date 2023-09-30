de84a06eb20f288143738142785a2ed637aea07a
Leonardo Wijaya <leonardo.wijaya003@binus.ac.id>
2023-09-30 22:42:20 +0700

    (fix) Styling

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -28,7 +28,38 @@
         </div>
     </main>
 
-    <x-landing.footer />
+    <footer class="mx-[120px] mt-32 flex flex-col pb-20">
+        <div class="flex flex-row text-base text-black space-x-32">
+            <div class="flex flex-col space-y-6 w-1/5">
+                <img src="{{ asset('images/logo-2.png') }}" alt="Logo 2" class="w-max" />
+                <p class="font-normal text-sm">OLPAIN 2023 adalah singkatan dari Olimpiade PAI Nasional AGPAII tahun
+                    2023 yang dilaksanakan secara berjenjang dari tingkat Provinsi hingga final tingkat Nasional</p>
+            </div>
+            <ul class="space-y-2">
+                <li>
+                    <a href="/" class="font-semibold">Beranda</a>
+                </li>
+                <li>
+                    <a href="/">Beranda</a>
+                </li>
+                <li>
+                    <a href="#informasi">Pusat Informasi</a>
+                </li>
+                <li><a href="#cara-pendaftaran">Cara Pendaftaran</a></li>
+            </ul>
+            <ul class="space-y-2">
+                <li>
+                    <a href="#" class="font-semibold">Contact Center</a>
+                </li>
+                <li><a href="/">Beranda</a></li>
+                <li><a href="#informasi">Pusat Informasi</a></li>
+                <li><a href="#cara-pendaftaran">Cara Pendaftaran</a></li>
+            </ul>
+        </div>
+        <div class="flex flex-row justify-end mt-20">
+            <h5 class="text-custom-grey text-xs">© Copyright 2023 | Olimpiade AGPAII </h5>
+        </div>
+    </footer>
 </body>
 
 </html>
