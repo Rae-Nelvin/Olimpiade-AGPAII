788ba1f24d9a7c5da228ea5068ed939438a54f48
Leonardo Wijaya <leonardo.wijaya003@binus.ac.id>
2023-10-02 13:14:04 +0700

    (fix) Styling

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -23,40 +23,7 @@
     </div>
     <div class="w-1/3 bg-white h-full"></div>
     <div class="absolute inset-0 z-10 m-[120px] grid grid-cols-12 gap-5">
-        <div class="col-span-5 flex flex-col text-white justify-between">
-            <a href="/" class="font-normal text-base"><- Beranda</a>
-                    <div class="flex flex-col space-y-6">
-                        <h2 class="font-normal text-xl">2023</h2>
-                        <h1 class="font-semibold text-6xl">Olimpiade PAI <br>Nasional AGPAII</h1>
-                    </div>
-                    <div class="flex flex-col">
-                        <h2 class="font-normal text-base mb-4">Organized by</h2>
-                        <div class="flex flex-col space-y-6">
-                            <div class="flex flex-row space-x-4 items-center">
-                                <img src="{{ asset('images/logo-7.png') }}" alt="Logo 7"
-                                    class="w-auto bg-white rounded-full py-1 px-2.5">
-                                <div class="flex flex-col text-sm space-y-1">
-                                    <h1 class="uppercase font-semibold">Smarta Indonesia</h1>
-                                    <h2 class="font-normal">Member of Puspresnas Kemdikbud RI</h2>
-                                </div>
-                            </div>
-                            <div class="flex flex-row space-x-4 items-center">
-                                <img src="{{ asset('images/logo-2.png') }}" alt="Logo 2"
-                                    class="w-14 bg-white py-3 px-2 rounded-full">
-                                <div class="flex flex-col text-sm space-y-1">
-                                    <h1 class="uppercase font-semibold">DPP AGPAII</h1>
-                                    <h2 class="font-normal">Asosiasi Guru Pendidikan Agama <br /> Islam Indonesia</h2>
-                                </div>
-                            </div>
-                        </div>
-                        <h2 class="font-normal text-base mb-4 mt-11">Butuh Bantuan?</h2>
-                        <div class="flex flex-col font-bold text-base space-y-2">
-                            <h2>+62857 0714 5129 <span class="font-normal">Khoirul Fahmi</span></h2>
-                            <h2>+62896 3031 1098 <span class="font-normal">Budi Sulistyo</span></h2>
-                        </div>
-                    </div>
-        </div>
-
+        <x-auth.leftSide />
         {{ $slot }}
     </div>
 </body>
