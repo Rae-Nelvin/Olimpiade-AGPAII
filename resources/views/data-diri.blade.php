40ea3ff5e5e071a46c1d287903caa52f2f2af0ee
Leonardo Wijaya <leonardo.wijaya003@binus.ac.id>
2023-10-30 12:05:54 +0700

    (feat) Participant Dashboard Data Diri
    ready for prod

--- resources/views/data-diri.blade.php
+++ resources/views/data-diri.blade.php
@@ -6,11 +6,13 @@
                 <x-admin.dataDiriTile type="text" label="NISN (Nomor Induk Siswa Nasional)"
                     value="{{ $result->nisn }}" />
                 <x-admin.dataDiriTile type="text" label="Nama Lengkap" value="{{ $result->user->name }}" />
+                <x-admin.dataDiriTile type="text" label="Asal Provinsi" value="{{ $result->user->province->name ?? '-' }}" />
                 <x-admin.dataDiriTile type="text" label="E-mail" value="{{ $result->user->email }}" />
                 <x-admin.dataDiriTile type="text" label="Kartu Pelajar" value="{{ $result->foto_kartu_pelajar }}" />
             </div>
             <div class="w-1/2 flex flex-col space-y-5">
                 <x-admin.dataDiriTile type="text" label="Asal Sekolah" value="{{ $result->asal_sekolah }}" />
+                <x-admin.dataDiriTile type="text" label="Nomor Handphone" value="{{ $result->user->phone_number }}" />
                 <x-admin.dataDiriTile type="password" label="Kata Sandi" value="{{ $result->user->password }}" />
                 <x-admin.dataDiriTile type="text" label="Bukti Pembayaran"
                     value="{{ $result->foto_bukti_pembayaran }}" />
